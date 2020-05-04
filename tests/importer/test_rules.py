8be4a9ce2d372efb67cd9f9d05dcf3f80e572d9f
Petri Huovinen <petri.huovinen@nokia.com>
2020-05-04 14:30:19 +0300

    Add importe rules test skeleton
    Change-Id: I730dc77341efb2f70cbcd2a2abafa26c1ea41acb

---
+++ tests/importer/test_rules.py
@@ -0,0 +1,23 @@
+from stagecrew.importer import (
+    Rules,
+    RecursiveExclude,
+    RecursiveInclude,
+    Include,
+    Exclude)
+
+
+def test_rules():
+    r = Rules()
+    r.add(RecursiveExclude('a'))
+    r.add(RecursiveInclude('a.b'))
+    r.add(Exclude('a.b.c'))
+    r.add(Include('b.c'))
+
+    assert not r.is_included('a')
+    assert r.is_included('a.b')
+    assert r.is_included('a.b.d')
+    assert not r.is_included('a.b.c')
+    assert r.is.included('a.b.c.d')
+    assert not r.is_included('a.b.d')
+    assert r.is_included('b.c')
+    assert not r.is_included('b.c.d')
